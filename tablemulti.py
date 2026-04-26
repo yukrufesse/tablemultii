22f9a8fa14bd6f96f97448a3934a9ca7abbb5ec2
yukrufesse <yusufkaymaz718@gmail.com>
2026-04-26 11:49:18 +0200

    Add files via upload

---
+++ tablemulti.py
@@ -0,0 +1,10 @@
+print("tu veut voir la table de quelle nombre")
+nmbr = int(input(" "))
+
+multiplication = 1
+
+while multiplication <=10:
+    print(nmbr, "x" ,multiplication, "=" ,mlt)
+    multiplication += 1
+    mlt =nmbr * multiplication
+    
